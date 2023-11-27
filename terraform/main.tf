2cc35bc82954fb1eef9bcd4bf24a7d4b1308d424
horiaoanadev <152086737+horiaoanadev@users.noreply.github.com>
2023-11-27 06:55:15 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "my_sg" {
   }
 }
 
-resource "aws_instance" "EC2Test" {
+resource "aws_instance" "EC2Test1" {
   ami             = "ami-06dd92ecc74fdfb36"
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.my_subnet.id
